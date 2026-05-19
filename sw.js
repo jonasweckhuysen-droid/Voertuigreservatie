94cca76807f27fb4c993f4f5caa8253bdf4416f6
jonasweckhuysen-droid <jonasweckhuysen@gmail.com>
2026-05-19 14:42:39 +0200

    Update service worker for vehicle reservation caching

--- sw.js
+++ sw.js
@@ -1,27 +1,20 @@
-// Brandweer Voertuigreservatie - Service Worker
-const CACHE_NAME = 'brandweer-v1';
-const ASSETS = ['./'];
+const CACHE_NAME = 'voertuigreservatie-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
 
-self.addEventListener('install', e => {
-  self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS).catch(() => {}))
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  // Network-first: always try live, fall back to cache
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
-
